26baead02ac5ccaf1d325d5a6e35f6c7cd0ac86f
Nathan-Gideon-cs <ltr532@york.ac.uk>
2026-03-30 13:48:21 +0000

    Updated the final iteration of Shape.java

--- Shape.java
+++ Shape.java
@@ -1,27 +1,18 @@
-public abstract class Shape { // we set the class to be abstract because we will not be creating any objects of the Shape class, but rather 
- //using it as a base class for other shapes (like Circle, Square, etc.) that will inherit from it and implement the abstract methods defined in the Shape class.
-    protected Coordinates position; // sets a private variable for the position of the shape using the Coordinates class
-    @SuppressWarnings("unused")
-    private int sides; // sets a private variable for the number of sides of the shape
+public abstract class Shape { // Abstract class to represent a generic shape, which will be extended by specific shape classes like Circle, Square, Rectangle, and Triangle. This class defines common properties and methods that all shapes will have, such as position, number of sides, area, perimeter, translation, scaling, and display functionality.
+    protected Coordinates position; // Protected field to store the position of the shape, which is accessible to subclasses but not to other classes outside the hierarchy.
+    protected int sides; // Protected field to store the number of sides of the shape, which is set by the subclasses when they call the superclass constructor.
 
-    public Shape(int sides, Coordinates position) {
-        //the constructor for the Shape class takes in an integer sides and a Coordinates object position as parameters. It initializes the sides variable with the provided value and sets the position variable to the given Coordinates object, which represents the location of the shape in a 2D space.
-        this.sides = sides;
-        this.position = position;
-     // initializes the position variable with the provided Coordinates object
-     }
-
-    public void translate(int dx, int dy) { // using The translate method from the Coordinates class to move the shape by a certain amount in the x and y directions. It takes in two integers, dx and dy, which represent the change in x and y coordinates respectively. The method then calls the translate method of the position variable (which is a Coordinates object) to update the coordinates of the shape accordingly.
-        position.translate(dx, dy); // Reuses Coordinate's logic! 
+    public Shape(int sides, Coordinates position) {//   Constructor to initialize the shape's number of sides and position, which will be called by the subclasses when they create specific shapes. This constructor sets the sides and position fields based on the parameters provided by the subclasses.
+        this.sides = sides;//(w3schools, n.d.)
+        this.position = position;// Initializes the sides and position fields with the values provided as parameters when a new shape is created. The sides parameter indicates how many sides the shape has (e.g., 0 for Circle, 4 for Square and Rectangle, 3 for Triangle), while the position parameter specifies where the shape is located in a 2D space using a Coordinates object.
     }
 
-    // Abstract methods to be overridden by children programs: set this method and all others to abstract since they will be different for each shape.
-    public abstract double getArea();
+    public void translate(int dx, int dy) {
+        position.translate(dx, dy); // Translates the shape by updating its position based on the provided dx and dy values, which represent the amount to move the shape in the x and y directions, respectively. This method calls the translate method of the Coordinates class to update the position of the shape accordingly.
+    }
 
-    public abstract double getPerimeter();
-
-    public abstract void scale(int factor, boolean sign);
-
-    public abstract String display();
-    
+    public abstract double getArea();// Abstract method to calculate the area of the shape, which must be implemented by each specific shape subclass (Circle, Square, Rectangle, Triangle) to provide the appropriate area calculation based on their unique properties and formulas. 
+    public abstract double getPerimeter();// Abstract method to calculate the perimeter of the shape, which must be implemented by each specific shape subclass to provide the appropriate perimeter calculation based on their unique properties and formulas.
+    public abstract void scale(int factor, boolean sign);// Abstract method to scale the shape, which must be implemented by each specific shape subclass to provide the appropriate scaling behavior based on their unique properties. The factor parameter indicates how much to scale the shape, while the sign parameter indicates whether to scale up (true) or down (false).
+    public abstract String display();// Abstract method to return a string representation of the shape, which must be implemented by each specific shape subclass to provide a meaningful description of the shape, including its type, position, dimensions, area, and perimeter as appropriate for that shape.
 }
